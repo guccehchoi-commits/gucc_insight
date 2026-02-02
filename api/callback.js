a85dd076ce84ea96608e513058f489246bcdeec1
GUCC <gucc.ehchoi@gmail.com>
2026-02-02 12:05:29 +0900

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,1 +1,35 @@
-export default async function handler(req, res) { res.send('인증 완료! 창을 닫고 관리자 페이지로 돌아가세요.'); }
+export default async function handler(req, res) {
+  const { code } = req.query;
+  const { GITHUB_ID, GITHUB_SECRET } = process.env;
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      client_id: GITHUB_ID,
+      client_secret: GITHUB_SECRET,
+      code,
+    }),
+  });
+
+  const data = await response.json();
+  const content = `
+    <html>
+      <body>
+        <script>
+          const postData = {
+            token: "${data.access_token}",
+            provider: "github"
+          };
+          window.opener.postMessage("authorizing:github", "*");
+          window.opener.postMessage('authorization:github:success:' + JSON.stringify(postData), '*');
+          window.close();
+        </script>
+      </body>
+    </html>
+  `;
+  res.send(content);
+}
